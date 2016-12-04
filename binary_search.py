a1e8270f0329a953da513d06409eaecdba5413e7
Jakubsamurai <jjanarek@gmail.com>
2016-12-04 13:59:35 +0100

    love algos

---
+++ binary_search.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def binary_search(list,item):
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = high + low
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
